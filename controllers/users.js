05459a408ffe55d831488cb88cd012094d1ef420
Spidey507 <miguelalveo@gmail.com>
2021-06-10 15:03:34 -0400

    Add find specific user controller

--- controllers/users.js
+++ controllers/users.js
@@ -16,6 +16,36 @@ var controller = {
       }
     })
 
+  },
+  find: (req, res) => {
+    var user_id = req.params.id
+    if (!user_id || user_id == null) {
+      return res.status(404).send({
+        status: 'error',
+        message: 'No has enviado el id del usuario.'
+      });
+    }
+    pool.query('SELECT * FROM employees where id = $1',
+      [user_id], (error, results) => {
+      if (error) {
+        //throw error
+        res.status(500).send({
+          message: 'Ya fue hubo un error',
+          error: error
+        });
+      } else {
+        var users = results.rows
+        if (users.length == 0) {
+          res.status(404).send({
+            status: 404,
+            message: 'El usuario solicitado no existe.'
+          });
+        } else {
+          res.status(200).json(users[0])
+        }
+      }
+    })
+
   }
 }
 
--- routes/routes.js
+++ routes/routes.js
@@ -6,5 +6,6 @@ var UsersController = require('../controllers/users');
 var router = express.Router();
 
 router.get('/users/all', UsersController.users_list);
+router.get('/user/:id', UsersController.find);
 
 module.exports = router;
